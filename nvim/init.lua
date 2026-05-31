a6ad493bb8781136420f741cf998ee0753bae4b0
Aundre Lattie <aundre@gmail.com>
2026-05-31 09:13:58 -0400

    added proper setup for nvim-lint to resolve error message

--- nvim/init.lua
+++ nvim/init.lua
@@ -159,11 +159,22 @@ require("lazy").setup({
 		-- linter
 		{
 			"mfussenegger/nvim-lint",
+			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
 			opts = {
 				linters_by_ft = {
 					go = { "golangci-lint" },
 				},
 			},
+			config = function(_, opts)
+				local lint = require("lint")
+				lint.linters_by_ft = opts.linters_by_ft
+
+				vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+					callback = function()
+						lint.try_lint()
+					end,
+				})
+			end,
 		},
 		-- rust plugin
 		{
